679817fb360287e7942732f730b713c3c1f44a0c
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-04 15:55:03 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 class Student < ActiveRecord::Base
+  after_initialize :set_defaults, unless: :persisted?
+    # The set_defaults will only work if the object is new
+
+    def set_defaults
+      self.active = false if self.active.nil?
+    end
+    
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def status
+    if self.active == true
+      @status = "active"
+    else
+      @status = "inactive"
+    end
+    @status
   end
 end
